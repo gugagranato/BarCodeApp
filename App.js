9055e68536de786f434e45667af616aa5fa99e81
gugagranato <gustavo.granato@hotmail.com>
2021-09-22 11:57:26 -0300

    feat: initial commig

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, TouchableOpacity } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
-  const [text, setText] = useState('Not yet scanned')
+  const [information, setInformation] = useState(false);
+  const [text, setText] = useState()
+
+  const bd = [
+    {
+      ean: 7898901646051,
+      name: 'Água mineral Karisa',
+      price: 'R$ 10,00'
+    },
+    {
+      ean: 7896048606938,
+      name: 'Creme de ureia',
+      price: 'R$ 11,00'
+    },
+    {
+      ean: 7298901641231,
+      name: 'Mostarda',
+      price: 'R$ 12,00'
+    }
+  ]
+
+  const bdMarket = [
+    {
+      ean: 7898901646051,
+      name: 'Água mineral Karisa',
+      price: 'R$ 10,00'
+    },
+    {
+      ean: 7896048606938,
+      name: 'Creme de ureia',
+      price: 'R$ 13,00'
+    },
+    {
+      ean: 7298901641231,
+      name: 'Mostarda',
+      price: 'R$ 12,00'
+    }
+  ]
 
   const askForCameraPermission = () => {
     (async () => {
@@ -22,8 +59,13 @@ export default function App() {
   // What happens when we scan the bar code
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
-    setText(data)
-    console.log('Type: ' + type + '\nData: ' + data)
+    const filterDataMarket = bdMarket.filter((item) => item.ean === +data)
+    const filterData = bd.filter((item) => item.ean === +data)
+    if(filterData[0].price !== filterDataMarket[0].price) {
+      setInformation(true)
+      setText(filterDataMarket)
+    }
+    setText(filterDataMarket)
   };
 
   // Check permissions and return the screens
@@ -41,6 +83,14 @@ export default function App() {
       </View>)
   }
 
+  const handleScanAgain = () => {
+    setScanned(false)
+    setInformation(false)
+    setText()
+  }
+  // useEffect(() => {
+  //   text && console.log(text, 'obj')
+  // }, [])
   // Return the View
   return (
     <View style={styles.container}>
@@ -49,9 +99,26 @@ export default function App() {
           onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
           style={{ height: 400, width: 400 }} />
       </View>
-      <Text style={styles.maintext}>{text}</Text>
+      {information ? (
+        <>
+          <Text style={styles.maintext}>EAN: {text[0].ean} </Text>
+          <Text style={styles.maintext}>Nome: {text[0].name} </Text>
+          
+          <Text style={styles.maintext}>Atenção: houve alteração no preço.</Text>
+          <Text style={styles.maintextbold}>O novo valor é {text[0].price}</Text>
+          <TouchableOpacity>
+            <Text style={{fontSize: 22, fontWeight: '600', marginBottom: 20}}>Atualizar</Text>
+          </TouchableOpacity>
+        </>
+      ): text ? (
+        <>
+          <Text style={styles.maintext}>EAN: {text[0].ean} </Text>
+          <Text style={styles.maintext}>Nome: {text[0].name} </Text>
+          <Text style={styles.maintext}>Preço: {text[0].price} </Text>
+        </>
+      ) : <Text>Nenhum produto escaneado</Text>}
 
-      {scanned && <Button title={'Scan again?'} onPress={() => setScanned(false)} color='tomato' />}
+      {scanned && <Button title={'Scan again?'} onPress={() => handleScanAgain()} color='blue' />}
     </View>
   );
 }
@@ -67,6 +134,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
     margin: 20,
   },
+  maintextbold: {
+    fontSize: 16,
+    margin: 20,
+    fontWeight: '700'
+  },
   barcodebox: {
     alignItems: 'center',
     justifyContent: 'center',
@@ -74,6 +146,6 @@ const styles = StyleSheet.create({
     width: 300,
     overflow: 'hidden',
     borderRadius: 30,
-    backgroundColor: 'tomato'
+    backgroundColor: 'red'
   }
 });
